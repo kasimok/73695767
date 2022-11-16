baa4a83ab943b7fa4696876435270f7832c886e8
kakaiikaka <kasimok@163.com>
2022-11-16 08:49:44 +0800

    Update Coordinator.swift

--- 73695767/Coordinator.swift
+++ 73695767/Coordinator.swift
@@ -2,13 +2,23 @@ import AVFoundation
 import Foundation
 
 class Coordinator {
-    
+
     let synthesizer: AVSpeechSynthesizer
     var player: AVAudioPlayer?
-    
+
     init() {
         let synthesizer = AVSpeechSynthesizer()
         self.synthesizer = synthesizer
+        
+        //Some debug commands
+//        debugPrint(AVSpeechSynthesisVoice(language: AVSpeechSynthesisVoice.currentLanguageCode()))
+//        debugPrint(AVSpeechSynthesisVoice(language: "en"))
+//        debugPrint(AVSpeechSynthesisVoice(language: "en-US"))
+//        debugPrint(AVSpeechSynthesisVoice(language: "en-GB"))
+//        debugPrint(AVSpeechSynthesisVoice(identifier: AVSpeechSynthesisVoiceIdentifierAlex))
+        debugPrint(AVSpeechSynthesisVoice.speechVoices())
+
+        debugPrint(recordingPath)
     }
     
     var recordingPath:  URL {
@@ -20,10 +30,10 @@ class Coordinator {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0].appendingPathComponent(soundName)
     }
-    
+
     func speakPhrase(phrase: String) {
         let utterance = AVSpeechUtterance(string: phrase)
-        utterance.voice = AVSpeechSynthesisVoice(language: "en")
+        utterance.voice = AVSpeechSynthesisVoice(language:  AVSpeechSynthesisVoiceIdentifierAlex)
         synthesizer.speak(utterance)
     }
     
@@ -36,8 +46,8 @@ class Coordinator {
             
             player = try AVAudioPlayer(contentsOf: recordingPath, fileTypeHint: AVFileType.caf.rawValue)
             guard let player = player else {return}
-            
-            player.play()
+                
+                player.play()
         } catch {
             print("Error playing file.")
         }
@@ -45,8 +55,8 @@ class Coordinator {
     
     func saveAVSpeechUtteranceToFile() {
         
-        let utterance = AVSpeechUtterance(string: "This is speech to record")
-        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        let utterance = AVSpeechUtterance(string: "This is speech to record, and are you fine with the result?")
+        utterance.voice = AVSpeechSynthesisVoice(language:  AVSpeechSynthesisVoiceIdentifierAlex)
         utterance.rate = 0.50
         
         // Only create new file handle if `output` is nil.
@@ -58,24 +68,22 @@ class Coordinator {
             }
             if pcmBuffer.frameLength == 0 {
                 // Done
+                debugPrint("Done")
             } else {
-                // this closure is called multiple times. so to save a complete audio, try create file handle only for once.
-                
+                // append buffer to file
                 do{
                     if output == nil {
                         try  output = AVAudioFile(
                             forWriting: recordingPath,
                             settings: pcmBuffer.format.settings,
-                            commonFormat: .pcmFormatInt16,
+                            commonFormat: .pcmFormatFloat32,
                             interleaved: false)
                     }
                     try output?.write(from: pcmBuffer)
                 }catch {
                     print(error.localizedDescription)
                 }
-                
             }
-            
         }
     }
 }
